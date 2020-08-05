eacd8838120cf985096be1ed1141480f21c6171a
Walaa Sbaih <walaasbaih@gmail.com>
2020-08-05 18:54:59 +0300

    back

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// app.get('/', (req, res) => res.send('API Running'));
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+ var items = require('../database-mysql');
+ var mysql = require('mysql');
+ var db = require("./../database-mysql/index")
+ var login = require("./regist")
+ var router = express.Router();
+ const bcrypt = require('bcrypt');
+//  const db = 'test'
+db.connection.connect(function(err) {
+  if (err) console.log(err)
+  console.log("Connected!");
+});
+app.get('/', function(req, res) {
+  res.send("hhh");
+});
+app.post('/registeruser',login.register);
+app.post('/loginuser',login.login);
+app.post('/registerowner',login.registerowner);
+app.post('/loginowner',login.loginowner);
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
@@ -14,5 +31,4 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
