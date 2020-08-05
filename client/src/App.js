e84f240c72cc7ca416b9d1ecfc437e136311d081
NeveenEl-Khozondar <neveen.khozondar11@gmail.com>
2020-08-05 18:54:59 +0300

    frontEnd

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+// import Abotamem from '../src/components/login/login';
+import Login from './components/login/login';
+import SignupOwner from './components/signupOwner/signupOwner';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SinCustomer from './components/signupCustomer/signupCustomer';
+
+// import CustumerPage from '../src/components/custumerPage/custumerPage'
+// import OwnerPage from '../src/components/ownerPage/ownerPage'
+// import RentPage from '../src/components/rentPage/rentPage'
+// import LandingPage from '../src/components/landingPage/landingPage'
+// import Calender from '../src/components/calender/calender'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <Route exact path='/' component={Login} />
+      <section>
+        <Route exact path='/signupCustomer' component={SinCustomer} />
+        <Route exact path='/signupOwner' component={SignupOwner} />
+        {/* <Route exact path='/custumerPage' component={CustumerPage}/>
+          <Route exact path='/ownerPage' component={OwnerPage}/>
+          <Route exact path='/rentPage' component={RentPage}/>
+          <Route exact path='/landingPage' component={LandingPage}/>
+          <Route exact path='/calender' component={Calender}/> */}
+      </section>
+    </Router>
   );
 }
 
