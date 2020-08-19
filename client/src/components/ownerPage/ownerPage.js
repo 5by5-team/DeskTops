5733388e0890ae52f42af21341a68c9de63452f0
Khalednaie <abo.naie.93@gmail.com>
2020-08-19 13:33:26 +0300

    solved

--- client/src/components/ownerPage/ownerPage.js
+++ client/src/components/ownerPage/ownerPage.js
@@ -23,7 +23,6 @@ export default function Ownerpage() {
 		console.log(email);
 	});
 
-<<<<<<< HEAD
 	useEffect(() => {
 		const fetchData = async () => {
 			const result = await axios.post('http://localhost:5000/getoffice', {
@@ -38,152 +37,73 @@ export default function Ownerpage() {
 	return (
 		<div>
 			<Navbar />
-			<div className='ow1'>
-				<Link href='/cal' onClick={console.log('kk')}>
-					<Button variant='contained' color='primary'>
-						CLENDER
-					</Button>
-				</Link>
-				<Link href='/addOffice' onClick={console.log('kk')}>
-					<Button variant='contained' color='primary'>
-						ADD OFFICE
-					</Button>
-				</Link>
-				<Link href='/bookingoffice' onClick={console.log('kk')}>
-					<Button variant='contained' color='primary'>
-						My Booking Office
-					</Button>
-				</Link>
-			</div>
-			<boot.Row className='rowOw'>
-				{data.map((element, index) => {
-					return (
-						<boot.Card
-							className='cardOw1'
-							key={index}
-							style={{ width: '18rem' }}
-						>
-							<boot.Card.Img variant='top' src={element.imgUrl} />
-							<Box component='fieldset' mb={3} borderColor='transparent'>
-								<Rating name='read-only' value={element.rating} readOnly />
-							</Box>
-							<boot.Card.Body>
-								<boot.Card.Title>{element.Discription} </boot.Card.Title>
-								<boot.Card.Text>{element.email}</boot.Card.Text>
-								<Button
-									variant='secondary'
-									onClick={() => {
-										office_id = element.office_id;
-										console.log(office_id);
-										axios
-											.post('http://localhost:5000/deletoff', { office_id })
-											.then(res => {
-												console.log(res.data);
-												window.location.reload(true);
-											})
-											.catch(err => {
-												console.log(err);
-											});
-									}}
-								>
-									DELETE
-								</Button>
-							</boot.Card.Body>
-						</boot.Card>
-					);
-				})}
-			</boot.Row>
+			<br />
+			<Link href='/cal' onClick={console.log('kk')}>
+				<Button variant='contained' color='primary'>
+					CLENDER
+				</Button>
+			</Link>
+			<Link href='/addOffice' onClick={console.log('kk')}>
+				<Button variant='contained' color='primary'>
+					ADD OFFICE
+				</Button>
+			</Link>
+			<Link href='/bookingoffice' onClick={console.log('kk')}>
+				<Button variant='contained' color='primary'>
+					My Booking Office
+				</Button>
+			</Link>
+			{data.map((element, index) => {
+				return (
+					<boot.Container>
+						<boot.Row>
+							<boot.Col>
+								<br />
+							</boot.Col>
+							<boot.Col>
+								<boot.Card key={index} style={{ width: '18rem' }}>
+									<boot.Card.Img variant='top' src={element.imgUrl} />
+									<boot.Card.Body>
+										<boot.Card.Title>{element.Discription} </boot.Card.Title>
+									</boot.Card.Body>
+								</boot.Card>
+								<br />
+							</boot.Col>
+							<boot.Col>
+								<boot.Card key={index} style={{ width: '18rem' }}>
+									<boot.Card.Img variant='top' src={element.imgUrl} />
+
+									<Rating name='read-only' value={element.rating} readOnly />
+
+									<boot.Card.Body>
+										<boot.Card.Title>{element.Discription} </boot.Card.Title>
+										<boot.Card.Text>{element.email}</boot.Card.Text>
+										<Button
+											variant='secondary'
+											onClick={() => {
+												office_id = element.office_id;
+												console.log(office_id);
+												axios
+													.post('http://localhost:5000/deletoff', { office_id })
+													.then(res => {
+														console.log(res.data);
+														window.location.reload(true);
+													})
+													.catch(err => {
+														console.log(err);
+													});
+											}}
+										>
+											DELETE
+										</Button>
+									</boot.Card.Body>
+								</boot.Card>
+								<br />
+							</boot.Col>
+						</boot.Row>
+					</boot.Container>
+				);
+			})}
 		</div>
 	);
 }
-=======
-    useEffect(() => {
-        
-        const fetchData = async () => {
-          const result = await axios.post(
-            'http://localhost:5000/getoffice',{
-                email
-            }
-          );
-        console.log(result.data)
-          setData(result.data.success);
-  
-        };
-     
-        fetchData(); 
-    }, []);
-    return( 
-        <div>
-            <Navbar/> 
-            <br/>
-    <Link href='/cal' onClick={console.log('kk')}>
-        <Button variant='contained' color='primary'>
-           CLENDER
-        </Button>
-    </Link>
-    <Link href='/addOffice' onClick={console.log('kk')}>
-        <Button variant='contained' color='primary'>
-            ADD OFFICE
-        </Button>
-    </Link>
-    <Link href='/bookingoffice' onClick={console.log('kk')}>
-        <Button variant='contained' color='primary'>
-            My Booking Office
-        </Button>
-    </Link>
-    {data.map((element, index) => {
-				return (
-                    <boot.Container>
-                    <boot.Row>
-                    
-                    <boot.Col>
-                    <br/>
-                    </boot.Col>
-                    <boot.Col>
-                        	<boot.Card  key = {index} style={{ width: '18rem' }}>
-						<boot.Card.Img variant='top' src={element.imgUrl} />
-						<boot.Card.Body>
-							<boot.Card.Title>{element.Discription} </boot.Card.Title>
-							
-						</boot.Card.Body>
-					</boot.Card>
-                    <br/>
-                    </boot.Col>
-                    <boot.Col>
-                    <boot.Card  key = {index} style={{ width: '18rem' }}>
-						<boot.Card.Img variant='top' src={element.imgUrl} />
-                   
-        <Rating name="read-only" value={element.rating} readOnly />
-      
-						<boot.Card.Body>
-							<boot.Card.Title>{element.Discription} </boot.Card.Title>
-							<boot.Card.Text>
-								{element.email}
-							</boot.Card.Text>
-                            < Button  variant="secondary" onClick={() => {
-                                office_id  = element.office_id;
-                                console.log(office_id)
-						axios
-						.post('http://localhost:5000/deletoff', {office_id})
-						.then((res) => {
-              console.log(res.data);
-              window.location.reload(true);
-    
-						})
-						.catch((err) => {
-						  console.log(err);
-            })
-                      }}   >
-            DELETE
-          </Button>
-						</boot.Card.Body>
-					</boot.Card>
-                    <br/>
-                    </boot.Col>
-            </boot.Row>
-            </boot.Container>
-				);
-			})}
-    </div>
-    )}
->>>>>>> ff3bd4d684edf0e4f0bccef576ecf3455bb78c79
